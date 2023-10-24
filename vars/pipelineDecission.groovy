44051335765ce3d1a7412fd863b699d89a512935
medamshiva20 <medamshiva20@gmail.com>
2023-10-24 22:01:45 +0530

    based on deployment platform get the pipeline and trigger

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,5 +1,18 @@
 #!groovy
 
 def decidePipeline(Map configMap){
-    echo "I need to take decission based on the map you sent"
+    application = configMap.get("application")
+    //Here we are getting nodeJSVM
+    switch(application) {
+        case 'nodeJSVM':
+             echo "application is Node JS and VM based"
+            //nodeJSVMCI(configMap)
+           break
+        case 'JavaVM':
+           JavaVMCI(configMap)
+           break
+        default:
+           error "Un recognised application"
+           break
+    }
 }
